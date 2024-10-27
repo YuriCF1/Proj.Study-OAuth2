660b2cd8eba950ae2654884e319adf333957949d
Yuri Cruz <yuricruzf@gmail.com>
2024-10-27 18:19:15 -0300

    Creating controller to send data

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,20 @@
 exports.showIndex = (req, res, next) => {
-    res.render('index')
-}
+  res.render("index");
+};
 
 exports.showPageSignUp = (req, res, next) => {
-    res.render('signUp')
-}
+  res.render("signUp");
+};
 
 exports.showMembersPage = (req, res) => {
-    res.render('members')
-}
+  res.render("members");
+};
 
 exports.get404Page = (req, res, next) => {
-    res.status(404).render('404')
-}
+  res.status(404).render("404");
+};
+
+exports.signup = (req, res, next) => {
+  const { username, email, password } = req.body;
+  console.log(username, email, password);
+};
