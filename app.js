d49d4b6f38438679a702595fc0bcdb84c5979492
Yuri Cruz <yuricruzf@gmail.com>
2024-11-03 19:32:30 -0300

    Changing secret

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes/routes')
 const mongoConnect = require('./util/database').mongoConnect
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
@@ -8,7 +9,7 @@ const port = 3000;
 const session = require('express-session')
 const sessionStorage = require('./util/sessionStorage')
 app.use(session({
-    secret: 'alura', //Assinar a sessão
+    secret: process.env.SECRET_SESSION, //Assinar a sessão
     resave: false, //Indica se a sessão deve ser salva no armazenamento, mesmo que não tenha sido moficado durante a solicitação 
     saveUninitialized: false,  //Indica se a sessão não inicializada deve ser salva no armazenamento
     // cookie: { secure: true }
